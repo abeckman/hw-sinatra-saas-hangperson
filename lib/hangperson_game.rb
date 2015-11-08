bb55570d0dac0a9ddf87c53724c36b5d8aa3bd08
Art Beckman <aabeckman@yahoo.com>
2015-11-08 04:02:17 +0000

    finished lib/hangerson_game.rb edits

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,6 +10,42 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
+  def guess(letter)
+    raise ArgumentError if letter.nil? || letter.empty? || letter[/[a-zA-Z]+/]  != letter
+    if @guesses.upcase.include?(letter.upcase) || @wrong_guesses.upcase.include?(letter.upcase)
+      return false
+    end
+    if @word.upcase.include?(letter.upcase)
+      @guesses += letter
+    else
+      @wrong_guesses += letter
+    end
+    return true
+  end
+
+  def word_with_guesses
+    display_word = ''
+    for i in 0..(@word.size - 1)
+      display_word += '-'
+    end
+    return display_word if @guesses.empty?
+    pattern = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.gsub(/[#{@guesses}]/, '')
+    display_word = @word.gsub(/[#{pattern}]/, '-')
+    return display_word
+  end
+
+  def check_win_or_lose
+    return :win if ((test_word = word_with_guesses) == @word)
+    return :lose if @wrong_guesses.size >= 7
+    return :play
   end
 
   def self.get_random_word
